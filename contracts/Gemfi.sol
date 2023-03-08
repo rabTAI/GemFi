bfaaeafeacf42365e053e45c3b6b24c44827af75
rabTAI <rabinofficial@hotmail.com>
2023-03-08 00:23:12 -0700

    Final contracts

--- contracts/Gemfi.sol
+++ contracts/Gemfi.sol
@@ -5,8 +5,6 @@ pragma solidity ^0.8.17;
 GemFi
 */
 
-import "hardhat/console.sol";
-
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
